3e855244e7c5b008f9cb573373fda1a566aa8ed0
Raphael Stoeckli <github@rabanti.ch>
2019-05-11 01:57:02 +0200

    More instrumentation
    This reverts commit 0ec6361c191b60bb4f8a19b187167ed95a3c79b0.

--- MXGTest/Relationships/XmlContentTypesTest.cs
+++ MXGTest/Relationships/XmlContentTypesTest.cs
@@ -13,28 +13,52 @@ namespace MXGTest.Relationships
     {
 
         [Description("Test the constructor of the XmlContentTypes class with the available properties")]
-        [TestCase( "NS1", 10, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"/>")]
+        [TestCase("NS1", 10, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"/>")]
         [TestCase("", 0, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"\"/>")]
         [TestCase(null, -5, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns/>")]
         public void ConstructorTest(string nameSpace, int estimatedElements, string expectedString)
         {
             XmlContentTypes types = new XmlContentTypes(nameSpace, estimatedElements);
 
-            Assert.Multiple(() => {
+            Assert.Multiple(() =>
+            {
                 Assert.IsNotNull(types);
                 Assert.AreEqual(types.GetXmlString(), expectedString);
             });
         }
 
         [Description("Test the AddDefaultContentType method")]
-        public void AddDefaultContentTypeTest(string contentType, string extension)
+        [TestCase("CT1", "EXT1", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Default ContentType=\"CT1\" Extension=\"EXT1\"/></Types>")]
+        [TestCase(null, "EXT2", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Default ContentType Extension=\"EXT2\"/></Types>")]
+        [TestCase("CT2", null, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Default ContentType=\"CT2\" Extension/></Types>")]
+        [TestCase(null, null, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Default ContentType Extension/></Types>")]
+        [TestCase("", "", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Default ContentType=\"\" Extension=\"\"/></Types>")]
+        public void AddDefaultContentTypeTest(string contentType, string extension, string expectedString)
         {
             XmlContentTypes types = new XmlContentTypes("NS1");
             types.AddDefaultContentType(contentType, extension);
-
+            Assert.Multiple(() =>
+            {
+                Assert.IsNotNull(types);
+                Assert.AreEqual(types.GetXmlString(), expectedString);
+            });
         }
 
-
-
+        [Description("Test the AddOverrideContentType method")]
+        [TestCase("CT1", "P1", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Override ContentType=\"CT1\" Extension=\"P1\"/></Types>")]
+        [TestCase(null, "P2", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Override ContentType Extension=\"P2\"/></Types>")]
+        [TestCase("CT2", null, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Override ContentType=\"CT2\" Extension/></Types>")]
+        [TestCase(null, null, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Override ContentType Extension/></Types>")]
+        [TestCase("", "", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Types xmlns=\"NS1\"><Override ContentType=\"\" Extension=\"\"/></Types>")]
+        public void AddOverrideContentTypeTest(string contentType, string partName, string expectedString)
+        {
+            XmlContentTypes types = new XmlContentTypes("NS1");
+            types.AddOverrideContentType(contentType, partName);
+            Assert.Multiple(() =>
+            {
+                Assert.IsNotNull(types);
+                Assert.AreEqual(types.GetXmlString(), expectedString);
+            });
+        }
     }
 }
